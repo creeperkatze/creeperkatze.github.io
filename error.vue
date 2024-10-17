20599974852eeb73b28cb8de6c59e48cc88b9a6d
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2024-10-17 18:12:28 +0200

    Revert to Nuxt 3 for now

---
+++ error.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      <h1>Even this cute cat couldn't find the page you were looking for ;(</h1>
+    </div>
+    <img src="public/soggy-cat-cat.gif" width="250"/>
+  </template>
