c0b57b7984f8627f351a4d54b2a692568a56f214
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2024-10-17 19:18:59 +0200

    Add link to the game on itch.io

--- pages/superslide.vue
+++ pages/superslide.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="content">
         <img src="~assets/images/superslide/GameTitle1024.png" width="800" class="center">
-        <p>Itch.io mirror version coming here soon...</p>
+        <p>Mirror version of the <a href="https://creeperkatze.itch.io/superslide">Itch.io game</a> coming here soon...</p>
         <a href="/">
             <h3>Home</h3>
         </a>
