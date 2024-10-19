6ab0592ef6f3680545aa7e43881da0d03ff2fbc6
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2024-10-19 22:12:21 +0200

    Update footer text size again

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="bg-black  items-center">
         <p class="text-white mt-4 mx-5 text-center">©<span v-text="new Date().getFullYear()"></span> Creeperkatze</p> 
-        <p class="text-white mx-5 text-center text-xs sm:text-base">Built with <a href="https://nuxt.com/" target="_blank">Nuxt</a> and <a href="https://tailwindcss.com/" target="_blank">Tailwind</a>. Source available on <a href="https://github.com/Creeperkatze/creeperkatze.github.io" target="_blank">GitHub</a>.</p>
+        <p class="text-white mx-5 text-center text-xm sm:text-base">Built with <a href="https://nuxt.com/" target="_blank">Nuxt</a> and <a href="https://tailwindcss.com/" target="_blank">Tailwind</a>. Source available on <a href="https://github.com/Creeperkatze/creeperkatze.github.io" target="_blank">GitHub</a>.</p>
     </footer>
 </template>
