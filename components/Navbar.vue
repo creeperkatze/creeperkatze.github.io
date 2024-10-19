6bb5fe417dde8bbaaadae38114014001918fe348
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2024-10-19 19:56:36 +0200

    Navbar improvements

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,18 +2,23 @@
     <nav class="bg-black p-4 mb-5" >
         <div class="container mx-auto flex items-center justify-between">
             <!-- Left Element -->
-            <a href="/" class="ml-20">
-                <img src="~assets/images/LogoBanner.png" class="pixelated">
+            <a href="/" class="lg:ml-60 ml-0">
+                <picture>
+                    <!-- Fallback for very small screens -->
+                    <source srcset="~/assets/images/Logo.png" media="(max-width: 640px)" />
+                    <!-- Default Logo Banner -->
+                    <img src="~/assets/images/LogoBanner.png" class="pixelated">
+                </picture>
             </a>
 
             <!-- Middle Elements -->
-            <div class="md:flex space-x-6 ml-4 mr-4">
+            <div class="md:flex space-x-6">
                 <a href="/" class="navbar-element">Home</a>
                 <a href="/superslide" class="navbar-element">Superslide</a>
             </div>
 
             <!-- Social Media Icons on Right (SVG) -->
-            <div class="flex space-x-4 mr-20"> <!-- Added a flex container for spacing -->
+            <div class="flex space-x-4 lg:mr-60 mr-0"> <!-- Added a flex container for spacing -->
                 <a href="https://www.instagram.com/creeperkatze/" target="_blank" class="navbar-social-media-icon">
                     <img src="~assets/images/external/instagram.svg" class="w-8 h-8"/>
                 </a>
@@ -27,3 +32,16 @@
         </div>
     </nav>
 </template>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const currentPath = window.location.pathname;
+        const navLinks = document.querySelectorAll('.navbar-element');
+        
+        navLinks.forEach(link => {
+            if (link.getAttribute('href') === currentPath) {
+                link.classList.add('active');
+            }
+        });
+    });
+</script>
