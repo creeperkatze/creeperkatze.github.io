99ee5c71d4eb78294ed6088784e40d1d8a613562
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2024-10-21 18:06:23 +0200

    Shiny new project cards

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,32 @@
         A Unity game developer from Germany, with my current big project being Superslide.<br>
         This website is still in early developement, and will change drastically over time.
     </p>
-    <h2 class="mb-2">My projects</h2>
-    <h4>
-        <ul class="list-disc">
-            <li><a href="/superslide">Superslide</a></li>
-        </ul>
-    </h4>
+    <h2 class="mb-5">My projects</h2>
+    <div class="grid grid-cols-2 gap-2 justify-center bg-center max-w-4xl mx-auto">
+        <div class="rounded-lg bg-black">
+            <a href="/superslide">
+                <img class="rounded-t-lg" src="~/assets/images/superslide/SplashBackgroundGameTitle.png" alt="SUPERSLIDE" />
+            </a>
+            <div class="p-5">
+                <a href="/superslide">
+                    <h3 class="mb-2 font-bold text-white">Superslide</h3>
+                </a>
+                <p class="mb-3 text-white">Slide through epic levels, spend your time in endless mode and climb the leaderboards!</p>
+            </div>
+        </div>
+        <div class="rounded-lg bg-black">
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+                <img class="rounded-t-lg" src="~/assets/images/CardComingSoon.png" alt="Coming Soon"/>
+            </a>
+            <div class="p-5">
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+                    <h3 class="mb-2 font-bold text-white">Coming Soon</h3>
+                </a>
+                <p class="mb-3 text-white"></p>
+            </div>
+        </div>
+        
+    </div>
 </template>
 
 <script setup lang="ts">
