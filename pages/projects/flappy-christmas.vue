c0dd8371680354b0f4d90e1bf172858f75dd374e
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2024-10-23 20:24:53 +0200

    Add itch.io widgets

--- pages/projects/flappy-christmas.vue
+++ pages/projects/flappy-christmas.vue
@@ -1,7 +1,6 @@
 <template>
     <img src="~/assets/images/flappy-christmas/FlappyChristmas.png" width="800" class="center mb-5">
-    <p class="center mb-5">Flappy Christmas on <a href="https://creeperkatze.itch.io/flappy-christmas">Itch.io</a>
-    </p>
+    <iframe class="center rounded-lg" frameborder="0" src="https://itch.io/embed/2438601?bg_color=FFFFFF&amp;fg_color=ff0000&amp;link_color=f5ce00&amp;border_color=bebebe" width="552" height="167"><a href="https://creeperkatze.itch.io/flappy-christmas">Flappy Christmas by Creeperkatze</a></iframe>
 </template>
 
 <script setup lang="ts">
--- pages/projects/superslide.vue
+++ pages/projects/superslide.vue
@@ -1,8 +1,10 @@
 <template>
     <img src="~/assets/images/superslide/GameTitle1024.png" width="800" class="center mb-5">
-    <p class="center mb-5">Mirror version of the <a href="https://creeperkatze.itch.io/superslide">Itch.io game</a>
-        coming here soon...
-    </p>
+    <iframe class="center rounded-lg" frameborder="0"
+        src="https://itch.io/embed/2402263?bg_color=ffffff&amp;fg_color=222222&amp;link_color=327345&amp;border_color=bebebe"
+        width="552" height="167"><a href="https://creeperkatze.itch.io/superslide">Superslide by
+            Creeperkatze</a></iframe>
+    <p class="center mt-5">Mirror version coming here soon...</p>
 </template>
 
 <script setup lang="ts">
