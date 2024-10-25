03812fe6dadb73087ea766cc1a2009a102006fb6
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2024-10-25 15:19:03 +0200

    Add width to coming soon project NuxtImg

--- components/Projects.vue
+++ components/Projects.vue
@@ -2,8 +2,7 @@
     <div class="flex flex-wrap justify-start gap-4 justify-items-center mx-auto max-w-[1000px]">
         <NuxtLink to="/projects/superslide" class="no-underline">
             <div class="rounded-lg bg-black transition ease-in-out hover:-translate-y-2 max-w-[450px] h-[400px]">
-                <NuxtImg format="webp" class="rounded-t-lg object-cover w-full"
-                    src="/images/superslide/SplashBackgroundGameTitle.png" alt="SUPERSLIDE" />
+                <NuxtImg src="/images/superslide/SplashBackgroundGameTitle.png" format="webp" width="450" class="rounded-t-lg object-cover w-full" alt="Superslide" />
                 <div class="p-5">
                     <h3 class="mb-2 font-bold text-white">Superslide</h3>
                     <p class="mb-3 text-white">{{ $t("project_descriptions.superslide")}}</p>
@@ -12,8 +11,7 @@
         </NuxtLink>
         <NuxtLink to="/projects/flappy-christmas" class="no-underline">
             <div class="rounded-lg bg-black transition ease-in-out hover:-translate-y-2 max-w-[450px] h-[400px]">
-                <NuxtImg format="webp" class="rounded-t-lg object-cover w-full"
-                    src="/images/flappy-christmas/Cover.png" alt="SUPERSLIDE" />
+                <NuxtImg src="/images/flappy-christmas/Cover.png" format="webp" width="450" class="rounded-t-lg object-cover w-full" alt="Flappy Christmas" />
                 <div class="p-5">
                     <h3 class="mb-2 font-bold text-white">Flappy Christmas</h3>
                     <p class="mb-3 text-white">{{ $t("project_descriptions.flappy_christmas")}}</p>
@@ -22,7 +20,7 @@
         </NuxtLink>
         <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" class="no-underline">
             <div class="rounded-lg bg-black transition ease-in-out hover:-translate-y-2 max-w-[450px] h-[400px]">
-                <NuxtImg format="webp" class="rounded-t-lg object-cover w-full" src="/images/CardComingSoon.png" alt="Coming Soon" />
+                <NuxtImg src="/images/CardComingSoon.png" format="webp" width="450" class="rounded-t-lg object-cover w-full" alt="Coming Soon" />
                 <div class="p-5">
                     <h3 class="mb-2 font-bold text-white">{{ $t("project_descriptions.coming_soon")}}</h3>
                 </div>
