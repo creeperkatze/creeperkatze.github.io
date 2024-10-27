1b0a92ad1a1d8cc8a7a1321f13ef60a740d1197a
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2024-10-27 15:34:03 +0100

    Reorder typing effect phrases

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <h2 class="mb-4">{{ $t("index.introduction") }}</h2>
-    <h1 class="mb-4"><TypingEffect :phrases='[ $t("index.typing_effect.unity_dev"), $t("index.typing_effect.mobile_game_dev"), $t("index.typing_effect.web_dev"), $t("index.typing_effect.pixel_artist"), $t("index.typing_effect.superslide_dev"), $t("index.typing_effect.stardew_modder"),]' :loop="true" /></h1>
+    <h1 class="mb-4"><TypingEffect :phrases='[ $t("index.typing_effect.unity_dev"), $t("index.typing_effect.mobile_game_dev"), $t("index.typing_effect.web_dev"), $t("index.typing_effect.stardew_modder"), $t("index.typing_effect.pixel_artist"), $t("index.typing_effect.superslide_dev"),]' :loop="true" /></h1>
     <p class="mb-4 whitespace-pre-line">
         {{ $t("index.welcome") }}
     </p>
