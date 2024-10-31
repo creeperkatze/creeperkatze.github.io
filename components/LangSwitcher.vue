ead820048db14049c213b51515652e5fc7efe904
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2024-10-31 14:07:06 +0100

    Improve LangSwitcher

--- components/LangSwitcher.vue
+++ components/LangSwitcher.vue
@@ -5,10 +5,11 @@ const availableLocales = computed(() =>
 {
     return locales.value.filter(i => i.code !== locale.value)
 })
+
 </script>
 
 <template>
-    <div v-for="locale in availableLocales" :key="locale.code" @click.prevent.stop="setLocale(locale.code)"
+    <div v-for="locale in availableLocales" :key="locale.code" @click.prevent.stop="() => setLocale(locale.code)"
         class="self-center hover:cursor-pointer">
         <img :src="`/images/flags/${locale.code}.svg`" alt="Language"
             class="w-8 rounded-lg border-2 border-white transition-transform duration-200 hover:scale-125" />
