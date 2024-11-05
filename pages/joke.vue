3b2dacc8260d39f19bfbd4a5d885e88eb7ef4f3e
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2024-11-05 23:30:23 +0100

    Improve layout of joke page

--- pages/joke.vue
+++ pages/joke.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex flex-col min-h-screen justify-center items-center">
+    <div class="flex flex-col justify-center items-center">
         <div class="rounded-lg border-gray-300 border-2 p-4 max-w-screen-sm">
-            <h1 class="mt-4">{{ joke }}</h1>
+            <h1>{{ joke }}</h1>
             <p class="mb-4" v-html="$t('joke.credits')"></p>
             <Button link="/joke" @click="fetchJoke" buttonTextKey="button.new_joke" :rainbowBackground="true"/>
         </div>
