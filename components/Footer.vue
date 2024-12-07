feb86068c2bfb5070d4b2fc67dd726d91073b015
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2024-12-07 20:32:39 +0100

    Exclude footer from snippet

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="bg-black items-center ignore-scrollbar pb-2 h-auto">
+    <footer class="bg-black items-center ignore-scrollbar pb-2 h-auto data-nosnippet">
         <p class="text-white mt-4 mx-4 text-center text-sm md:text-lg">
             ©<span v-text="new Date().getFullYear()"></span> {{ $t("footer.copyright") }}
         </p>
