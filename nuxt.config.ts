330377b74197d03462d7ca56113765a3758bf198
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2024-12-20 00:17:33 +0100

    Try to fix canonical links

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,7 +38,8 @@ export default defineNuxtConfig({
 
     site: {
         url: "https://creeperkatze.github.io",
-        name: "Creeperkatze"
+        name: "Creeperkatze",
+        trailingSlash: true,
     },
 
     i18n: {
