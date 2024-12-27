b3029891f44d5cce0b6829ae47c98a24874ea79f
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2024-12-27 14:42:11 +0100

    Add issue template links to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,6 @@
 <template>
-    <footer class="bg-black items-center max-w-screen pb-2 h-auto" style="padding-left: calc(100vw - 100%);" data-nosnippet>
+    <footer class="bg-black items-center max-w-screen pb-2 h-auto" style="padding-left: calc(100vw - 100%);"
+        data-nosnippet>
         <p class="text-white mt-4 mx-4 text-center text-sm md:text-lg">
             ©<span v-text="new Date().getFullYear()" /> {{ $t("footer.copyright") }}
         </p>
@@ -7,10 +8,10 @@
             {{ $t("footer.text_part1") }}
             <a href="https://nuxt.com/" target="_blank">{{
                 $t("footer.link_nuxt")
-                }}</a>{{ $t("footer.text_part2")
+            }}</a>{{ $t("footer.text_part2")
             }}<a href="https://tailwindcss.com/" target="_blank">{{
                 $t("footer.link_tailwind")
-                }}</a>{{ $t("footer.text_part3")
+            }}</a>{{ $t("footer.text_part3")
             }}<a href="https://github.com/creeperkatze/creeperkatze.github.io" target="_blank">{{
                 $t("footer.link_github") }}</a>.
         </p>
@@ -22,5 +23,11 @@
             <img class="h-6 px-1 inline"
                 src="https://img.shields.io/github/actions/workflow/status/Creeperkatze/creeperkatze.github.io/deploy.yml?logo=github"
                 alt="Build Status"></a>
+        <a :href="$t('footer.badge.problem.link')">
+            <img class="h-6 px-1 inline" :src="$t('footer.badge.problem.shield')"
+                :alt="$t('footer.badge.problem.alt')"></a>
+        <a :href="$t('footer.badge.suggestion.link')">
+            <img class="h-6 px-1 inline" :src="$t('footer.badge.suggestion.shield')"
+                :alt="$t('footer.badge.suggestion.alt')"></a>
     </footer>
 </template>
