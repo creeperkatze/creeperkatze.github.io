f57ab3a02e0659f27d2ea746692a630c247081d6
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2025-03-03 22:06:09 +0100

    Improve mobile home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <h2 class="mb-4">{{ $t("page.index.introduction") }}</h2>
-    <h1 class="mb-4"><TypingEffect :phrases='[ $t("page.index.typing_effect.unity_dev"), $t("page.index.typing_effect.mobile_game_dev"), $t("page.index.typing_effect.web_dev"), $t("page.index.typing_effect.stardew_modder"), $t("page.index.typing_effect.pixel_artist"), $t("page.index.typing_effect.superslide_dev"),]' :loop="true" /></h1>
+    <h2 class="mb-4 text-2xl sm:text-3xl">{{ $t("page.index.introduction") }}</h2>
+    <h1 class="mb-4 text-3xl sm:text-4xl"><TypingEffect :phrases='[ $t("page.index.typing_effect.unity_dev"), $t("page.index.typing_effect.mobile_game_dev"), $t("page.index.typing_effect.web_dev"), $t("page.index.typing_effect.stardew_modder"), $t("page.index.typing_effect.pixel_artist"), $t("page.index.typing_effect.superslide_dev"),]' :loop="true" /></h1>
     <p class="mb-4 whitespace-pre-line">
         {{ $t("page.index.welcome") }}
     </p>
