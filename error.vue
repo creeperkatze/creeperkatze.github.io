9a95fcaa80c6f55d7a87527eb4051689c45aeda7
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2025-04-16 18:17:26 +0200

    Fix error.vue layout formatting

--- error.vue
+++ error.vue
@@ -1,15 +1,17 @@
 <template>
-    <NuxtLayout />
-    <h1 class="text-red-600">{{ error?.statusCode }}</h1>
-    <h5 class="text-red-600 mb-5">{{ error?.statusMessage }}</h5>
-    <h2 v-if="error?.statusCode === 404" class="mb-5">
-        {{ $t("page.error.404") }}
-    </h2>
-    <h2 v-else class="mb-5">
-        {{ $t("page.error.else") }}
-    </h2>
-    <img src="~/assets/images/error/soggy-cat-cat.gif" :alt="$t('page.error.image.cat')" class="center w-[400px] mb-5">
-    <Button link="/">{{ $t('button.home') }}</Button>
+    <NuxtLayout>
+        <h1 class="text-red-600">{{ error?.statusCode }}</h1>
+        <h5 class="text-red-600 mb-5">{{ error?.statusMessage }}</h5>
+        <h2 v-if="error?.statusCode === 404" class="mb-5">
+            {{ $t("page.error.404") }}
+        </h2>
+        <h2 v-else class="mb-5">
+            {{ $t("page.error.else") }}
+        </h2>
+        <img src="~/assets/images/error/soggy-cat-cat.gif" :alt="$t('page.error.image.cat')"
+            class="center w-[400px] mb-5">
+        <Button link="/">{{ $t('button.home') }}</Button>
+    </NuxtLayout>
 </template>
 
 <script setup>
