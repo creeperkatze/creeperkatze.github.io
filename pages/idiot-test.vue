96a8800b8fc034ec33fca1e4fec80efdde5add06
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2025-07-11 01:33:25 +0200

    Improve idiot test progress bar

--- pages/idiot-test.vue
+++ pages/idiot-test.vue
@@ -4,8 +4,8 @@
         <p class="mb-4 whitespace-pre-wrap">{{ $t("page.idiot_test.text") }}</p>
         <div v-if="!allQuestionsAnswered"
             class="sticky top-24 max-w-xl mx-auto p-4 glass-effect border-gray-400 border-2 rounded-lg mb-4 z-10">
-            <div class="w-full bg-gray-200 h-2 rounded-full">
-                <div class="bg-neutral-600 h-2 rounded-full" :style="{ width: percentageAnswered + '%' }" />
+            <div class="w-full bg-neutral-600 h-2 rounded-full">
+                <div class="bg-white h-2 rounded-full" :style="{ width: percentageAnswered + '%' }" />
             </div>
             <p class="mt-2 text-center">
                 {{ $t("page.idiot_test.progress") }}{{ percentageAnswered }}%
