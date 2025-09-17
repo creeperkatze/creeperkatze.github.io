1a0991fdbf2a62302efcac85966f019fe8e05084
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2025-09-17 18:22:12 +0200

    Add border to ads

--- app/components/GoogleAd.vue
+++ app/components/GoogleAd.vue
@@ -2,30 +2,29 @@
 import { useRuntimeConfig } from 'nuxt/app';
 import { onMounted } from 'vue'
 
-interface Props {
-  adSlot: string
-  adStyle: string
-  responsive: boolean
+interface Props
+{
+    adSlot: string
+    adStyle: string
+    responsive: boolean
 }
 
 const props = defineProps<Props>()
 const runtimeConfig = useRuntimeConfig()
 
-onMounted(() => {
-  if (typeof window !== 'undefined') {
-    ;(window as any).adsbygoogle = (window as any).adsbygoogle || []
-    ;(window as any).adsbygoogle.push({})
-  }
+onMounted(() =>
+{
+    if (typeof window !== 'undefined')
+    {
+        ; (window as any).adsbygoogle = (window as any).adsbygoogle || []
+            ; (window as any).adsbygoogle.push({})
+    }
 })
 </script>
 
 <template>
-  <ins
-    class="adsbygoogle"
-    :style="props.adStyle"
-    :data-ad-client="runtimeConfig.public.adSenseId"
-    :data-ad-slot="props.adSlot"
-    data-ad-format="auto"
-    :data-full-width-responsive="responsive"
-  />
+    <div class="rounded-lg border-2">
+        <ins class="adsbygoogle" :style="props.adStyle" :data-ad-client="runtimeConfig.public.adSenseId"
+            :data-ad-slot="props.adSlot" data-ad-format="auto" :data-full-width-responsive="responsive" />
+    </div>
 </template>
