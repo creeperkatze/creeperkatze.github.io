980f24044d12d5f3d084d33d693a0cb0ee3d451a
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2025-09-18 16:46:15 +0200

    dont use custom class

--- app/components/GoogleAd.vue
+++ app/components/GoogleAd.vue
@@ -4,7 +4,6 @@ import { onMounted, ref } from 'vue'
 
 interface Props {
   adSlot: string
-  customClass?: string
 }
 
 const props = defineProps<Props>()
@@ -34,7 +33,7 @@ onMounted(() => {
 <template>
   <div
     class="w-full max-w-[800px] mx-auto overflow-hidden rounded-lg p-2"
-    :class="[props.customClass, adLoaded ? 'border-2' : '']"
+    :class="adLoaded ? '' : 'hidden'"
   >
     <ins
       class="adsbygoogle block w-full h-[100px]"
