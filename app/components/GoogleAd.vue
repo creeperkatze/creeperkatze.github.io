5cce8f0a438ac02c932a850aac8629770364bc2b
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2025-09-18 16:35:31 +0200

    Only show ad if loaded

--- app/components/GoogleAd.vue
+++ app/components/GoogleAd.vue
@@ -21,14 +21,19 @@ onMounted(() =>
 </script>
 
 <template>
-<div class="w-full max-w-[800px] mx-auto overflow-hidden border-2 rounded-lg p-2">
-  <ins
-    class="adsbygoogle block w-full h-[100px]"
-    style="display:block;"
-    :data-ad-client="runtimeConfig.public.adSenseId"
-    :data-ad-slot="props.adSlot"
-    data-ad-format="auto"
-    data-full-width-responsive="true">
-  </ins>
-</div>
+    <div class="w-full max-w-[800px] mx-auto overflow-hidden rounded-lg p-2">
+        <ins class="adsbygoogle block w-full h-[100px]" style="display:block;"
+            :data-ad-client="runtimeConfig.public.adSenseId" :data-ad-slot="props.adSlot" data-ad-format="auto"
+            data-full-width-responsive="true">
+        </ins>
+    </div>
 </template>
+
+<style>
+.ad-wrapper:has(ins iframe)
+{
+    border: 2px solid #000;
+    border-radius: 0.5rem;
+    padding: 0.5rem;
+}
+</style>
