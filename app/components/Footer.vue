74860722d793892eb849eb4c419d780a84080007
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2025-12-09 15:22:44 +0100

    Change footer, fix localization

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -7,24 +7,9 @@
         </p>
         <p class="text-white mb-2 mx-4 text-center text-sm md:text-lg">
             <NuxtLink href="/privacy" class="text-white">{{ $t("page.privacy.title") }}</NuxtLink>
-        </p>
-        <p class="text-white mb-2 mx-4 text-center text-sm md:text-lg">
+            <span class="mx-2">|</span>
             <a href="https://status.creeperkatze.de" class="text-white">{{ $t("footer.status") }}</a>
         </p>
-        <a href="https://github.com/creeperkatze/website/commits/main" target="_blank">
-            <img class="h-6 px-1 inline"
-                src="https://img.shields.io/github/last-commit/creeperkatze/website/main?display_timestamp=author&logo=github"
-                alt="Last Commit"></a>
-        <a href="https://github.com/creeperkatze/website/actions" target="_blank">
-            <img class="h-6 px-1 inline"
-                src="https://img.shields.io/github/actions/workflow/status/Creeperkatze/website/deploy.yml?logo=github"
-                alt="Build Status"></a>
-        <a :href="$t('footer.badge.problem.link')" target="_blank">
-            <img class="h-6 px-1 inline" :src="$t('footer.badge.problem.shield')"
-                :alt="$t('footer.badge.problem.alt')"></a>
-        <a :href="$t('footer.badge.suggestion.link')" target="_blank">
-            <img class="h-6 px-1 inline" :src="$t('footer.badge.suggestion.shield')"
-                :alt="$t('footer.badge.suggestion.alt')"></a>
     </footer>
 </template>
 
