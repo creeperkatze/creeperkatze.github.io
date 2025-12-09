b906578117afa75a2168f634109c1310376d378c
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2025-12-09 15:16:09 +0100

    Add status page

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,11 +1,15 @@
 <template>
-    <footer class="absolute pb-4 bottom-0 left-0 right-0 glass-effect border-gray-400 border-t-2 shadow-[rgba(0,0,0,0.1)_0px_10px_10px_20px]" style="padding-left: calc(100vw - 100%);"
-        data-nosnippet>
+    <footer
+        class="absolute pb-4 bottom-0 left-0 right-0 glass-effect border-gray-400 border-t-2 shadow-[rgba(0,0,0,0.1)_0px_10px_10px_20px]"
+        style="padding-left: calc(100vw - 100%);" data-nosnippet>
         <p class="text-white mt-4 mx-4 text-center text-sm md:text-lg">
             ©<span v-text="currentYear" /> {{ $t("footer.copyright") }}
         </p>
         <p class="text-white mb-2 mx-4 text-center text-sm md:text-lg">
             <NuxtLink href="/privacy" class="text-white">{{ $t("page.privacy.title") }}</NuxtLink>
+        </p>
+        <p class="text-white mb-2 mx-4 text-center text-sm md:text-lg">
+            <a href="https://status.creeperkatze.de" class="text-white">{{ $t("footer.status") }}</a>
         </p>
         <a href="https://github.com/creeperkatze/website/commits/main" target="_blank">
             <img class="h-6 px-1 inline"
